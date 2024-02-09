204f737d34e3cb8df8b4541c5f68393299b5cd1f
ShivamIT23 <shivam.gupta.23112003@gmail.com>
2024-02-09 13:30:01 +0530

    adding features

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
 let ans = document.getElementById("Answer");
 let ques = document.getElementById("questiontext");
 let button = document.getElementById("button1");
-button.onclick = function(){
+let form = document.getElementById("Guess");
+let check = document.getElementById("button2");
+function gamers(){
 let random =  Math.floor(Math.random()*101);
 let n;
 let i=1;
-
-ques.innerHTML = "Guess the number &rarr";
-  do{
-  if(n<random){
-    alert("Go higher! , You can do it");
-    i++;
-  }
-  else if(n>random){
-    alert("Go Lower , You can do it");
-    i++;
-  }
-}while(n!=random);
-alert("Congratulation! You Guessed it correctly , Your score is " + (100-i));  
-console.log("Congratulation! You Guessed it correctly in " + i + " times.");  
+form.hidden =false;
+check.hidden = false;
+ques.innerHTML = `Guess the number ---->`;
+//   do{
+//   if(n<random){
+//     alert("Go higher! , You can do it");
+//     i++;
+//   }
+//   else if(n>random){
+//     alert("Go Lower , You can do it");
+//     i++;
+//   }
+// }while(n!=random);
+// alert("Congratulation! You Guessed it correctly , Your score is " + (100-i));  
+// console.log("Congratulation! You Guessed it correctly in " + i + " times.");  
 }
+button.addEventListener("click",gamers);
